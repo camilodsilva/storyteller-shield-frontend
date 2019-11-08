b604bc4c29e13993d5ea4f42d1a0397b113fe7c3
CamiloSilvaD <camilodsmp@gmail.com>
2019-11-07 22:30:21 -0300

    Update the server file correcting the Express static file serving

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const path = require("path");
 const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`$/dist/$`));
+app.use(express.static(`dist/${nomeApp}`));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(`$/dist/$/index.html`));
+  console.log(path.join(`dist/${nomeApp}/index.html`));
+  res.sendFile(path.join(`dist/${nomeApp}/index.html`));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
